715941781ca984c7025600d92227d01847fd25d4
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-03-26 04:06:24 +0600

    footer section done

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,7 @@
 
 const Blogs = () => {
     return (
-        <div className="min-h-[calc(100vh-224px-112px)] flex justify-center items-center">
+        <div className="min-h-[calc(100vh-256px-112px)] flex justify-center items-center">
 
             <div>
                 <h1 className="text-5xl font-semibold text-gray-500">No Blogs Available Now!!</h1>
